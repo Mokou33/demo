11c1a103dcfc346550e303d8dffc453b1e502686
scarlet-moon <978542146@qq.com>
2019-04-12 16:38:17 +0800

    case

--- src/components/case/index.jsx
+++ src/components/case/index.jsx
@@ -13,34 +13,34 @@ class Case extends Component {
                 </div>
                 <div className="con">
                     <ul className="conUl">
-                        <li><img src={require("../../assets/img/case/01.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/02.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/03.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/04.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/06.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/08.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/10.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/12.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/14.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/15.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/17.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/19.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/20.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/21.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/22.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/23.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/25.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/30.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/33.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/35.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/38.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/39.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/40.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/41.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/45.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/46.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/47.jpg")} alt=""/></li>
-                        <li><img src={require("../../assets/img/case/48.jpg")} alt=""/></li>
+                        <li><img src={require("../../assets/img/case/01.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/02.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/03.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/04.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/06.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/08.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/10.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/12.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/14.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/15.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/17.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/19.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/20.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/21.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/22.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/23.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/25.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/30.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/33.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/35.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/38.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/39.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/40.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/41.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/45.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/46.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/47.jpg")} alt="case"/></li>
+                        <li><img src={require("../../assets/img/case/48.jpg")} alt="case"/></li>
 
                     </ul>
                     <div className="caseMore">
